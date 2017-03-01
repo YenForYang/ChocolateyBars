75bf0f96dd45efb95568d0b07b0081308531523a
YenForYang <richard@yenforyang.com>
2017-03-01 16:15:47 -0600

    Finishing Touches

--- rclone-beta/tools/chocolateyinstall.TempPoint.ps1
+++ rclone-beta/tools/chocolateyinstall.TempPoint.ps1
@@ -3,11 +3,7 @@
 $packageName= 'rclone-beta'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://beta.rclone.org/rclone-beta-latest-windows-386.zip'
-$url64 = 'http://beta.rclone.org/rclone-beta-latest-windows-amd64.zip'
-
-$html = $(Invoke-Webrequest -Uri 'http://beta.rclone.org/')
-$html.ParsedHtml.getElementsByTagName('TD') | Select-Object -Last 1 -Skip 5 | % { $checksum = $_.innerText }
-$html.ParsedHtml.getElementsByTagName('TD') | Select-Object -Last 1 | % { $checksum64 = $_.innerText }
+$url64      = 'http://beta.rclone.org/rclone-beta-latest-windows-amd64.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -16,13 +12,13 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  softwareName  = 'rclone*'
-	
-  checksum      = $checksum
+  softwareName  = 'rclone*' 
 
+  checksum      = '3362f1cafd7cc33e651b16959ca55e4d'
   checksumType  = 'md5'
-  checksum64    = $checksum64
-  checksumType64= 'md5'	
+  checksum64    = 'e313520b9430ed1a211e8769d38ef168'
+  checksumType64= 'md5'
+	
 }
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
