0825a6a625554d1890ee2eb98e6c40f88cab9d4a
YenForYang <richard@yenforyang.com>
2018-09-16 22:16:25 -0500

    2018 again

--- cfr-decompiler/update.ps1
+++ cfr-decompiler/update.ps1
@@ -1,10 +1,11 @@
 import-module au
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-function global:au_SearchReplace {
+function global:au_SearchReplace
+{
 	@{
-		'tools\chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"       = "`$1'$($Latest.URL32)'"
+		'tools\chocolateyInstall.ps1'  = @{
+			"(^[$]url\s*=\s*)('.*')"	    = "`$1'$($Latest.URL32)'"
 			"(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
 		}
 	}
@@ -15,13 +16,13 @@ function global:au_GetLatest
 	$html = Invoke-Webrequest -UseBasicParsing -Uri 'http://www.benf.org/other/cfr/' -Method Get
 	$s = $html.RawContent
 	$i = $s.IndexOf('href="cfr_') + 10
-	$version = $s.Substring($i,5)
-	if ($s.Substring($i+5,4) -ne '.jar' -or !$version) {return}
+	$version = $s.Substring($i, 5)
+	if ($s.Substring($i + 5, 4) -ne '.jar' -or !$version) { return }
 	
 	$url = "http://www.benf.org/other/cfr/cfr_${version}.jar"
-		
+	
 	
 	@{ URL32 = $url; Version = $version.Replace('_', '.'); ChecksumType32 = 'md5' }
 }
 
-update -ChecksumFor all -NoReadme -Force -NoCheckChocoVersion -NoCheckUrl -Verbose
+update -ChecksumFor 32 -NoReadme -Force -NoCheckChocoVersion -NoCheckUrl -Verbose
